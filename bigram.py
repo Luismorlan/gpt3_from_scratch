8794a39a3dbaaa54b88e6242bcd234d56a6f3c0c
warren <chenweilunster@gmail.com>
2023-10-14 11:58:49 -0700

    finished bigram

--- bigram.py
+++ bigram.py
@@ -6,10 +6,11 @@ from torch.nn import functional as F
 # hyperparameters
 batch_size = 32
 block_size = 8
-max_iters = 3000
+max_iters = 10000
 eval_interval = 300
 learning_rate = 1e-2
 device = "cuda" if torch.cuda.is_available() else "cpu"
+eval_iters = 200
 
 # ---------
 
@@ -29,7 +30,7 @@ decode = lambda l: "".join([itos[i] for i in l])
 data = torch.tensor(encode(text), dtype=torch.long)
 n = int(0.9 * len(data))
 train_data = data[:n]
-val_data = data[n:]
+eval_data = data[n:]
 
 
 def get_batch(split):
@@ -69,4 +70,51 @@ class BigramLanguageModel(nn.Module):
 
     def forward(self, idx, targets=None):
         logits = self.token_embedding_table(idx)
-        
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B * T, C)
+            targets = targets.view(B * T)
+            loss = F.cross_entropy(logits, targets)
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        # idx is (B,T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            logits, loss = self(idx)  # Logits here is of shape (B, T, C)
+            logits = logits[
+                :, -1, :
+            ]  # Take only the last in the context so it transform to (B, C)
+            probs = F.softmax(logits, dim=-1)  # (B, C)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)  # Concatenate on T's dimension
+        return idx
+
+
+model = BigramLanguageModel(vocab_size)
+m = model.to(device)
+
+# Create a PyTorch optimizer
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+for iter in range(max_iters):
+    # Every once in a while we report the loss
+    if iter % eval_interval == 0:
+        losses = estimate_loss()
+        print(
+            f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}"
+        )
+
+    xb, yb = get_batch("train")
+
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+print("generating...")
+context = torch.zeros((1, 1), dtype=torch.long, device=device)
+print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
+print("finished!")
